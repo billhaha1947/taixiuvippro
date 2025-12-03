c16df242ddc1399b4485b4eb74dae0e6570c1543
billhaha1947 <dangnhatminh18122008@gmail.com>
2025-12-03 15:14:45 +0700

    Update constants.js

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -33,24 +33,6 @@ export const CHAT_ROOMS = {
   GLOBAL: 'global'
 };
 
-export const SOUND_EFFECTS = {
-  DICE_ROLL: 'dice-roll',
-  DICE_SLAM: 'dice-slam',
-  CHIP_PLACE: 'chip-place',
-  WIN: 'win',
-  LOSE: 'lose',
-  COUNTDOWN: 'countdown',
-  BACKGROUND: 'background'
-};
-
-export const AVATAR_PLACEHOLDERS = [
-  'https://api.dicebear.com/7.x/avataaars/svg?seed=1',
-  'https://api.dicebear.com/7.x/avataaars/svg?seed=2',
-  'https://api.dicebear.com/7.x/avataaars/svg?seed=3',
-  'https://api.dicebear.com/7.x/avataaars/svg?seed=4',
-  'https://api.dicebear.com/7.x/avataaars/svg?seed=5'
-];
-
 export const COLORS = {
   TAI_BLACK: '#000000',
   XIU_WHITE: '#ffffff',
